758a7d0320c131692f2798e65d5c9c9e696061a2
reyrizki <reynaldirizki97.rr@gmail.com>
2021-03-15 14:49:09 +0700

    init: Initial codes
    - Copy existing codes
    - Ignore .editorconfig file

---
+++ W1/1/Comission.java
@@ -0,0 +1,9 @@
+//********************************************************************
+// Comission.java Author: Rey Rizki
+//
+// Hourly employee but with commission
+//********************************************************************
+public class Comission extends Hourly
+{
+
+}
