23e521fff30eb4d7766a896a2ef6fb804f0f757c
reyrizki <reynaldirizki97.rr@gmail.com>
2021-03-16 21:07:23 +0700

    update: Add testing for commission

--- W1/1/Staff.java
+++ W1/1/Staff.java
@@ -11,16 +11,62 @@ public class Staff
     //-----------------------------------------------------------------
     public Staff ()
     {
-        staffList = new StaffMember[6];
-        staffList[0] = new Executive ("Sam", "123 Main Line", "555-0469", "123-45-6789", 2423.07);
-        staffList[1] = new Employee ("Carla", "456 Off Line", "555-0101", "987-65-4321", 1246.15);
-        staffList[2] = new Employee ("Woody", "789 Off Rocker", "555-0000", "010-20-3040", 1169.23);
-        staffList[3] = new Hourly ("Diane", "678 Fifth Ave.", "555-0690", "958-47-3625", 10.55);
-        staffList[4] = new Volunteer ("Norm", "987 Suds Blvd.", "555-8374");
-        staffList[5] = new Volunteer ("Cliff", "321 Duds Lane", "555-7282");
+        staffList = new StaffMember[8];
+        staffList[0] = new Executive(
+            "Sam",
+            "123 Main Line",
+            "555-0469",
+            "123-45-6789",
+            2423.07
+        );
+        staffList[1] = new Employee(
+            "Carla",
+            "456 Off Line",
+            "555-0101",
+            "987-65-4321",
+            1246.15
+        );
+        staffList[2] = new Employee(
+            "Woody",
+            "789 Off Rocker",
+            "555-0000",
+            "010-20-3040",
+            1169.23
+        );
+        staffList[3] = new Hourly(
+            "Diane",
+            "678 Fifth Ave.",
+            "555-0690",
+            "958-47-3625",
+            10.55
+        );
+        staffList[4] = new Volunteer("Norm", "987 Suds Blvd.", "555-8374");
+        staffList[5] = new Volunteer("Cliff", "321 Duds Lane", "555-7282");
+        staffList[6] = new Comission(
+            "Ice Juice",
+            "111 Dude Lane",
+            "555-7222",
+            "123-32-2909",
+            6.25,
+            .2
+        );
+        staffList[7] = new Comission(
+            "Reina Rizka",
+            "222 Atlantis",
+            "555-7232",
+            "124-35-2999",
+            9.75,
+            .15
+        );
+
         ((Executive)staffList[0]).awardBonus (500.00);
         ((Hourly)staffList[3]).addHours (40);
+        ((Comission) staffList[6]).addHours(35);
+        ((Comission) staffList[6]).addSales(400);
+        ((Comission) staffList[7]).addHours(40);
+        ((Comission) staffList[7]).addSales(950);
     }
+
     //-----------------------------------------------------------------
     // Pays all staff members.
     //-----------------------------------------------------------------
